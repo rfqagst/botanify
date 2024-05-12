a7cb1ff58ff5a62dd7cb89429aa51c4b82523ae7
daffayusa <111202113462@mhs.dinus.ac.id>
2024-05-13 01:35:16 +0700

    Adding On Boarding Screen

--- app/src/main/java/com/example/botanify/screen/components/ButtonComponents.kt
+++ app/src/main/java/com/example/botanify/screen/components/ButtonComponents.kt
@@ -1,2 +1,43 @@
 package com.example.botanify.screen.components
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.botanify.ui.theme.BotanifyTheme
+
+@Composable
+fun PrimaryButton(
+    text: String,
+    onClick: () -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF2EB24D),
+            contentColor = Color.White
+        ),
+        shape = RoundedCornerShape(size = 6.dp)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ButtonPrev() {
+    BotanifyTheme {
+        PrimaryButton(text = "Selanjutnya") {
+
+        }
+    }
+}
